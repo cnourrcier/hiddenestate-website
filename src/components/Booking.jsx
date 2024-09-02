3acc8b0afa59bd5466dab3500d1c7a1fdb9c5cc7
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-09-02 14:38:31 -0400

    Add History button to booking component

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -5,7 +5,10 @@ const Booking = () => {
     return (
         <section className="booking">
             <h2>Book Now</h2>
-            <button className="book-button">Reserve Your Stay</button>
+            <div className='button-container'>
+                <button className="button history-button">View Our History</button>
+                <button className="button book-button">Reserve Your Stay</button>
+            </div>
         </section>
     )
 }
