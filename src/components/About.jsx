5fca1b2772263dead50353dde24e1f2febc11e99
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-09-03 11:47:52 -0400

    Updated styles in about, booking, features, and fadingcategories sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,23 @@
 import './About.css';
 
 const About = () => {
-
     return (
         <section className="about">
             <h2>About the Estate</h2>
-            <p>
-                Clark Gable, famous for his role as Rhett Butler in "Gone with the Wind," owned this estate with his wife Carole Lombard during the 1930s and 1940s. Known as the "desert hideaway," the property boasts many luxurious amenities typical of the era, including sprawling gardens, a large swimming pool, and expansive views of the San Jacinto Mountains.
+            <p className="about__subtitle">
+                Experience Clark Gable's Palm Springs Retreat that has been lovingly restored
             </p>
-            <blockquote>
-                "We love a good pink house, but a rosy-hued home that once belonged to one of Hollywood's dreamiest actors makes us especially giddy. It's like the real-estate equivalent of a strapping gentleman in a pink polo shirt — oddly, it just works." <br />- Lauran Piro, GOOD HOUSEKEEPING Oct 2, 2014
+            <div className="about__content">
+                <p>
+                    Clark Gable, famous for his role as Rhett Butler in "Gone with the Wind," owned this estate with his wife Carole Lombard during the 1930s and 1940s. Known as the "desert hideaway," the property boasts many luxurious amenities typical of the era, including sprawling gardens, a large swimming pool, and expansive views of the San Jacinto Mountains.
+                </p>
+                <p>
+                    Located just a couple of hours from Los Angeles, Palm Springs offered a convenient escape from the hustle and bustle of Hollywood. It provided a serene retreat where stars could unwind and enjoy a more private lifestyle within the tranquility of the desert.
+                </p>
+            </div>
+            <blockquote className="about__quote">
+                <p>"We love a good pink house, but a rosy-hued home that once belonged to one of Hollywood's dreamiest actors makes us especially giddy. It's like the real-estate equivalent of a strapping gentleman in a pink polo shirt — oddly, it just works."</p>
+                - Lauran Piro, GOOD HOUSEKEEPING Oct 2, 2014
             </blockquote>
         </section>
     )
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,18 +1,29 @@
 import './Features.css';
 
 const Features = () => {
-
     return (
         <section className="features">
-            <h2>Features</h2>
-            <ul>
+            <h2>Palm Springs Home for Events</h2>
+            <p className="features__subtitle">
+                Vacation Rental, Corp. Events, Weddings, Reunions, Dinner Parties, Film, Photography, Garden Parties, Private Tours
+            </p>
+            <div className="features__description">
+                <p>
+                    The perfect Palm Springs vacation / event home. A custom built 5 bedroom, 6 bath estate situated on nearly an acre lot with over 3300 square feet of luxury living.
+                </p>
+                <p>
+                    The Oldest home in Old Las Palmas, established in 1925. The Spanish Colonial Revival estate was Created by Master Architect McNeal Swasey. The estate was constructed during the height of the Spanish Revival architectural movement, a common design choice in the Palm Springs area during the 1920s and 1930s, reflecting the glamourous lifestyle of Hollywood's golden age.
+                </p>
+            </div>
+            <h3 className="features__subheading">Features</h3>
+            <ul className="features__list">
                 <li>Amazing Location: Walking distance to downtown Palm Springs</li>
-                <li>Mountain Views: Breathtaking views of San Jacinto Mountains</li>
-                <li>Expansive Grounds: Set on a large lot (nearly an acre)</li>
-                <li>Large Saltwater Swimming Pool and Pool Cabana</li>
-                <li>Pickle Ball Court</li>
-                <li>Private Casita above Garage</li>
-                <li>Gated, Private and Secluded</li>
+                <li>Mountain Views: The estate offers breathtaking views of San Jacinto Mountains, a signature feature of many luxury estates in Old Las Palms</li>
+                <li>Expansive Grounds: The property is set on a Large lot (nearly an acre), with lush gardens, mature palm trees, and ample outdoor space</li>
+                <li>Large Saltwater Swimming Pool and Pool Cabana with outdoor kitchen/BBQ facilities: Central features of the estate, the pool area is designed for both leisure and entertaining guests</li>
+                <li>Pickle Ball Court: Surrounded by mature date palms, ancient Olive trees and breathtaking views, is adaptable for hosting various outdoor events</li>
+                <li>Private Casita above Garage: Perfect location for the bride on wedding day or just relaxing on the second-floor deck admiring the pool area and mountains</li>
+                <li>Gated, Private and Secluded: Many event parking options</li>
             </ul>
         </section>
     )
