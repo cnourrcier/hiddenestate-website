cad60dee95ad87905d59a004b497cbb8d00d19f6
cnourrcier <cnourrcier@hotmail.com>
2024-12-16 20:44:22 -0500

    Homepage and header style updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,8 @@ const About = () => {
                 Welcome to the "Hidden Gable" 
                 <span className="about__location">Located in Old Las Palmas, Palm Springs</span>
             </h1>
-            <h2 className="about__subtitle">
-                Transform yourself back in time as though it happened one night, but make it your night in a celebrity Palm Springs Estate.
-            </h2>
             <div className="about__wrapper">
-                <div className="about__image-placeholder">
+                <div className="about__image-container">
                     <img 
                         src="/img/Gable-Lombard-39.jpg" 
                         alt="Clark Gable and Carole Lombard's Palm Springs Estate" 
@@ -21,10 +18,13 @@ const About = () => {
                 </div>
                 <div className="about__content">
                     <p>
-                        Clark Gable, famous for his role as Rhett Butler in <span className='movie_title'>"Gone with the Wind"</span>, owned this estate with his wife Carole Lombard during the 1930s and 1940s. Known as the <i>"desert hideaway"</i>, the property boasts many luxurious amenities typical of the era, including sprawling gardens, a large swimming pool, and expansive views of the San Jacinto Mountains.
+                        Clark Gable, famous for his role as Rhett Butler in <span className='movie_title'>"Gone with the Wind"</span> and his Oscar winning role in <span className='movie_title'>“It happened one night”</span>, occupied this estate with his wife Carole Lombard. Known as their <i>"desert hideaway"</i>, the property boasts many luxurious amenities typical of the era, including sprawling gardens, a large swimming pool, and expansive views of the San Jacinto Mountains.
                     </p>
                     <p>
                         Located just a couple of hours from Los Angeles, Palm Springs offered a convenient escape from the hustle and bustle of Hollywood. It provided a serene retreat where stars could unwind and enjoy a more private lifestyle within the tranquility of the desert.
+                    </p>
+                    <p className="about__subtitle">
+                        Transform yourself back in time as though it happened one night, but make it your night in a celebrity Palm Springs Estate.
                     </p>
                 </div>
             </div>
