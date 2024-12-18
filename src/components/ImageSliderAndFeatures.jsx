73685f703ae558907ed80fb1c944efcc8094ca4b
cnourrcier <cnourrcier@hotmail.com>
2024-12-17 21:30:40 -0500

    Main page style revisions

--- src/components/ImageSliderAndFeatures.jsx
+++ src/components/ImageSliderAndFeatures.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
-import './ImageSlider.css';
+import './ImageSliderAndFeatures.css';
 
 const ImageSlider = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     useEffect(() => {
         const timer = setInterval(() => {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 2) % images.length);
+            setCurrentImageIndex((prevIndex) => (prevIndex + 3) % images.length);
         }, 3000);
         return () => clearInterval(timer);
     }, []);
@@ -23,15 +23,23 @@ const ImageSlider = () => {
     ];
 
     return (
-        <section className="img-slider-section">
+        <section className="img-slider-and-features-section">
             <div className="img-slider">
                 <img src={images[currentImageIndex]} alt={`Resort view ${currentImageIndex + 1}`} />
                 <img src={images[(currentImageIndex + 1) % images.length]} alt={`Resort view ${(currentImageIndex + 1) % images.length} + 1`} />
+                <img src={images[(currentImageIndex + 2) % images.length]} alt={`Resort view ${(currentImageIndex + 2) % images.length + 1}`} />
             </div>
-            <div className="property__description">
-                <p>
-                    One of the oldest homes in Old Las Palmas. The Spanish Colonial Revival estate was Created by <span>Master Architect McNeal Swasey</span>. The estate was constructed during the height of the Spanish Revival architectural movement, a common design choice in the Palm Springs area during the 1920s and 1930s, reflecting the glamourous lifestyle of Hollywood's golden age.
-                </p>
+            <div className="features">
+                <h2 className="features__heading">Features</h2>
+                <ul className="features__list">
+                    <li><span className='feature'>Amazing Location:</span> Walking distance to downtown Palm Springs</li>
+                    <li><span className='feature'>Mountain Views:</span> The estate offers breathtaking views of San Jacinto Mountains, a signature feature of many luxury estates in Old Las Palms</li>
+                    <li><span className='feature'>Expansive Grounds:</span> The property is set on a Large lot (nearly an acre), with lush gardens, mature palm trees, and ample outdoor space</li>
+                    <li><span className='feature'>Large Saltwater Swimming Pool and Pool Cabana with outdoor kitchen/BBQ facilities:</span> Central features of the estate, the pool area is designed for both leisure and entertaining guests</li>
+                    <li><span className='feature'>Pickle Ball Court:</span> Surrounded by mature date palms, ancient Olive trees and breathtaking views, is adaptable for hosting various outdoor events</li>
+                    <li><span className='feature'>Private Casita above Garage:</span> Perfect location for the bride on wedding day or just relaxing on the second-floor deck admiring the pool area and mountains</li>
+                    <li><span className='feature'>Gated, Private and Secluded:</span> Many event parking options</li>
+                </ul>
             </div>
         </section>
     )
