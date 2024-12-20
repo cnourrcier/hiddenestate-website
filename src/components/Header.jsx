bde63549da0b5cbd813f0643b8f6d62f6a42248b
cnourrcier <cnourrcier@hotmail.com>
2024-12-20 15:25:46 -0500

    update header rental tab dropdown menu to show on hover instead of click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,12 @@ const Header = () => {
     const location = useLocation();
     const isMainPage = location.pathname == '/';
 
-    function handleToggleRentalMenuDropdown() {
-        setRentalMenuDropdown(prevState => !prevState);
+    function handleMouseEnter() {
+        setRentalMenuDropdown(true);
+    }
+
+    function handleMouseLeave() {
+        setRentalMenuDropdown(false);
     }
 
 
@@ -28,18 +32,22 @@ const Header = () => {
                 <Link to="/history" className='tab'>History</Link> 
                 {/* <Link to="#" className='tab'>Rental</Link> */}
 
-                <div className='rental-menu-dropdown-container'>
-                        <div onClick={handleToggleRentalMenuDropdown} className='tab'>
-                            <p>Rental</p>
-                        </div>
-                        <div className={`rental-menu-dropdown ${rentalMenuDropdown ? 'show' : ''}`}>
-                            <ul>
-                                <li onClick={handleToggleRentalMenuDropdown}><Link to="#" className='tab dropdown-tab'>Corporate Events</Link></li>
-                                <li onClick={handleToggleRentalMenuDropdown}><Link to="#" className='tab dropdown-tab'>Private Events</Link></li>
-                                <li onClick={handleToggleRentalMenuDropdown}><Link to="#" className='tab dropdown-tab'>Weddings</Link></li>
-                                <li onClick={handleToggleRentalMenuDropdown}><Link to="#" className='tab dropdown-tab'>Vacation Rentals</Link></li>
-                            </ul>
-                        </div>
+                <div 
+                    onMouseEnter={handleMouseEnter} 
+                    onMouseLeave={handleMouseLeave}
+                    className='rental-menu-dropdown-container'
+                >
+                    <div className='tab'>
+                        <p>Rental</p>
+                    </div>
+                    <div className={`rental-menu-dropdown ${rentalMenuDropdown ? 'show' : ''}`}>
+                        <ul>
+                            <li><Link to="#" className='tab dropdown-tab'>Corporate Events</Link></li>
+                            <li><Link to="#" className='tab dropdown-tab'>Private Events</Link></li>
+                            <li><Link to="#" className='tab dropdown-tab'>Weddings</Link></li>
+                            <li><Link to="#" className='tab dropdown-tab'>Vacation Rentals</Link></li>
+                        </ul>
+                    </div>
                 </div>
 
                 <Link to="/gallery" className='tab'>Gallery</Link>
