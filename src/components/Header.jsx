73043b8c3232b5c3fac3a2100543f70763f5f494
cnourrcier <cnourrcier@hotmail.com>
2024-12-20 15:17:58 -0500

    initial dropdown menu with onclick display

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
-    const location = useLocation();
+    const [rentalMenuDropdown, setRentalMenuDropdown] = useState(false);
 
+    const location = useLocation();
     const isMainPage = location.pathname == '/';
+
+    function handleToggleRentalMenuDropdown() {
+        setRentalMenuDropdown(prevState => !prevState);
+    }
+
 
     return (
         <header className={isMainPage ? 'header transparent' : 'header solid'}>
@@ -20,7 +26,22 @@ const Header = () => {
             <div className='tabs-container'>
                 <Link to="/" className='tab'>Home</Link>
                 <Link to="/history" className='tab'>History</Link> 
-                <Link to="#" className='tab'>Rental</Link>
+                {/* <Link to="#" className='tab'>Rental</Link> */}
+
+                <div className='rental-menu-dropdown-container'>
+                        <div onClick={handleToggleRentalMenuDropdown} className='tab'>
+                            <p>Rental</p>
+                        </div>
+                        <div className={`rental-menu-dropdown ${rentalMenuDropdown ? 'show' : ''}`}>
+                            <ul>
+                                <li onClick={handleToggleRentalMenuDropdown}><Link to="#" className='tab dropdown-tab'>Corporate Events</Link></li>
+                                <li onClick={handleToggleRentalMenuDropdown}><Link to="#" className='tab dropdown-tab'>Private Events</Link></li>
+                                <li onClick={handleToggleRentalMenuDropdown}><Link to="#" className='tab dropdown-tab'>Weddings</Link></li>
+                                <li onClick={handleToggleRentalMenuDropdown}><Link to="#" className='tab dropdown-tab'>Vacation Rentals</Link></li>
+                            </ul>
+                        </div>
+                </div>
+
                 <Link to="/gallery" className='tab'>Gallery</Link>
                 <Link to="#" className='tab'>Media</Link>
                 <Link to="/mission" className='tab'>Our Mission</Link>
