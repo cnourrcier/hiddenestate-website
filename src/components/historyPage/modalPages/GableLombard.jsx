a5eca0e5b4caa15769a64d046c2447fa4f16a350
cnourrcier <cnourrcier@hotmail.com>
2025-01-13 10:08:17 -0500

    Update GableLombard modal

--- src/components/historyPage/modalPages/GableLombard.jsx
+++ src/components/historyPage/modalPages/GableLombard.jsx
@@ -5,8 +5,8 @@ const GableLombard = () => {
     return (
         <div className='gable-lombard'>
             <div className='img one'>
-                <img  src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1735422116/Hidden%20Gable%20Estate/history%20page/RoaringTwenties_ik20wu.jpg`} alt="" />
-                <p className='img-description'></p>
+                <img src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736701593/Hidden%20Gable%20Estate/history%20page/Carole_Lombard_Cinelandia_August_1928_vlzwah.jpg`} alt="" />
+                <p className='img-description'>1928</p>
             </div>
             <p>
             Hidden Gable Estate, commissioned by Agnes Swobdi, is a product of the roaring 20's.  It was a time of great wealth creation, women's liberation and revealing fashions aka “flappers”.  It was also the golden age of Hollywood dominated by Clark Gable “King of Hollywood”. 
@@ -14,20 +14,16 @@ const GableLombard = () => {
             <p>
             Silent films became popular in the US at the beginning of the 20th century around the same time Swobdi had her first Millinery store (1901) in Los Angeles.  She evolved (with Hollywood) into high end couture to the silent movie stars.  Most likely provided fashions for yet to be discovered silent movie actress Carol Lombard who was later known as one of greatest actresses of Classic Hollywood Cinema.  Swobdi's parties in Los Angeles were legendary and certainly included rising stars and Hollywood's elite. 
             </p>
-            <div className='img two'>
-                <img src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736701593/Hidden%20Gable%20Estate/history%20page/Carole_Lombard_Cinelandia_August_1928_vlzwah.jpg`} alt="" />
-                <p className='img-description'>1928</p>
-            </div>
             <p>
             Clark Gable and Carol Lombard both got their starts as extras and supporting roles in silent films produced in the 20's (Clark Gable's in 1924 and Carol Lombard's in 1921).  Stars of the silent film era like Mary Pickford, Douglas Fairbanks, Sr., and Harold Lloyd faded into obscurity in the 1930s after the advent of the talkies but not Clark Gable and Carol Lombard.
             </p>
-            <p>
-            Gable and Lombard began a secret relationship that became public once his divorce from Langham was finalized in 1938.  Gable and Lombard (his true love) married soon after on March 12, 1939. 
-            </p>
-            <div className='img three'>
+            <div className='img two'>
                 <img src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736701592/Hidden%20Gable%20Estate/history%20page/Clark_Gable_Carole_Lombard_and_Lombard_s_mother_1939_lhosha.jpg`} alt="" />
                 <p className='img-description'>Clark Gable, Carol Lombard and Lombard's mother 1939</p>
             </div>
+            <p>
+            Gable and Lombard began a secret relationship that became public once his divorce from Langham was finalized in 1938.  Gable and Lombard (his true love) married soon after on March 12, 1939. 
+            </p>
             <p>
             Their primary residence (purchased just before they married) was a 20-acre ranch in the city of Encino, less than 15 miles from the recording studios in Culver City.  However, they had a second home in Palm Springs (Hidden Gable Estate) until Carol Lombard's untimely death in 1942.  This was their most private desert hideaway.
             </p>
@@ -37,7 +33,7 @@ const GableLombard = () => {
             <p>
             Doris Fleeson, a future resident of Hidden Gable Estate, meet Clark Gable in London.  You can see the article she wrote about Gable in her section of the property's history.
             </p>
-            <div className='img four'>
+            <div className='img three'>
                 <img src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736701592/Hidden%20Gable%20Estate/history%20page/Gable_Remembers_Lombard__1951_illustration__-_The_American_Weekly_lcyyem.jpg`} alt=""/>
                 <p className='img-description'>Gable Remembers Lombard 1951 illustration, The American Weekly</p>
             </div>
@@ -47,7 +43,7 @@ const GableLombard = () => {
             <p>
             One of the main attractions of Palm Springs and the greater Coachella Valley was the dry climate. Daily life was leisurely and centered around pools and golf courses. There were many good restaurants, but residents tended to entertain casually in their homes. It was a perfect place for the film elite to go when they were not making a movie, to relax and be “just normal folk”. Yet, near enough to the filming studios for a quick return if needed.  It offered as neighbors some of the Gables' good friends—William Powell, Kirk Douglas, William Holden, actress Brenda Marshall; Cary Grant; and Zeppo and Barbara Marx (who would later marry Frank Sinatra).
             </p>
-            <div className='img five'>
+            <div className='img four'>
                 <img src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736701592/Hidden%20Gable%20Estate/history%20page/Clark_Gable_plaque_-_Liberty_Theatre_wrqh1i.jpg`} alt="" />
                 <p className='img-description'>Liberty Theatre Plaque</p>
             </div>
