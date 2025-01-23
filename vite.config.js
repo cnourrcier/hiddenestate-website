4c9867e332b4c2714a8741675f0b097bccdeea32
cnourrcier <cnourrcier@hotmail.com>
2025-01-23 15:06:46 -0500

    Troubleshoot _redirects

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {},
+    },
+  },
+  publicDir: 'public',
   plugins: [react()],
 })
