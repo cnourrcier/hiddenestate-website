5c418744af9bcba51587d9db9535515f83577254
cnourrcier <cnourrcier@hotmail.com>
2025-03-17 00:24:43 -0400

    clean up HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,16 +12,6 @@ import ClaudeMonet from "../components/homePage/modalPages/ClaudeMonet";
 import { Helmet } from "react-helmet-async";
 import './HomePage.css';
 
-
-// const images = [
-//   { id: 1, url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1734804922/Hidden%20Gable%20Estate/home%20page/5750_y8gi72.jpg`, alt: 'Outdoor cabana seating area'},
-//   { id: 2, url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1734719452/Hidden%20Gable%20Estate/home%20page/5837_g7sld1.jpg`, alt: 'Saltwater pool with Outdoor Kitchen'},
-//   { id: 3, url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1734719349/Hidden%20Gable%20Estate/home%20page/5734_dy1q44.jpg`, alt: 'Interior living space with fireplace image 1'},
-//   { id: 4, url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1734719350/Hidden%20Gable%20Estate/home%20page/5742_mr4ibo.jpg`, alt: 'Interior living space with fireplace image 2'},
-//   { id: 5, url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1734719454/Hidden%20Gable%20Estate/home%20page/5757_ufhw0g.jpg`, alt: 'Pool and pickleball court with breathtaking views'},
-//   { id: 6, url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1737503845/Hidden%20Gable%20Estate/home%20page/0363_fb5ypn.jpg`, alt: 'Outdoor view of casita with palms'},
-// ];
-
 const images = [
   { id: 1, url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1741378878/Hidden%20Gable%20Estate/home%20page/_I1A0391_xn500l.jpg`, alt: 'Image 1'},
   { id: 2, url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1734804922/Hidden%20Gable%20Estate/home%20page/5750_y8gi72.jpg`, alt: 'Outdoor cabana seating area'},
@@ -30,12 +20,11 @@ const images = [
 ];
 
 const monetModalItem = {
+  galleryTitle: ["Claude Monet Inspiration"],
+  slug: "monet-inspiration",
   HTMLTitle: "Claude Monet Inspiration",
   modalTitle: "Claude Monet Inspiration",
-  slug: "monet-inspiration",
   image: claudeMonetInspiration,
-  galleryTitle: ["Claude Monet Inspiration"],
-  info: "The estate interior and exterior colors have been meticulously restored as they existed in the twenties. Swobdi, a frequent world traveler, got her inspiration and color pallet choices from Claude Monet's pink home in Giverny (a commune in France). Pink exterior walls with green shutters, yellow crown molding, exterior tile with all Monet's favorite colors, etc. Monet lived in his pink home from 1883 until his death in 1926. Another example of Monet's influence is his painting \"House Among the Palms\" with pink exterior and red tile roof.",
   Component: ClaudeMonet
 };
  
