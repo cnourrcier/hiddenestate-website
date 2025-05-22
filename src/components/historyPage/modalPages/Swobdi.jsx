5aceb47ce9b6a0a320428a501b0e7f4e9c1a0d9f
cnourrcier <cnourrcier@hotmail.com>
2025-05-22 15:21:28 -0500

    Update styles home page reviews section

--- src/components/historyPage/modalPages/Swobdi.jsx
+++ src/components/historyPage/modalPages/Swobdi.jsx
@@ -1,122 +1,131 @@
-import Carousel from '../../common/carousel/Carousel';
-import './Swobdi.css';
+import Carousel from "../../common/carousel/Carousel";
+import "./Swobdi.css";
 
 const Swobdi = () => {
-
     const images = [
-        { 
-            id: 1, 
+        {
+            id: 1,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874699/Hidden%20Gable%20Estate/history%20page/swobdi-fashions_j2in6j.jpg`,
             text: `"Julanne Johnson, leading woman for Douglas Fairbanks, has beauty which may well be likened to the most beautiful in art"`,
             text2: `Los Angeles Times 1924`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 2, 
+        {
+            id: 2,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874700/Hidden%20Gable%20Estate/history%20page/swobdi-with-glove_hwp2tj.jpg`,
             text: `Ruth Clifford, she was also, for a time, the voice of Walt Disney's Minnie Mouse and Daisy Duck`,
             text2: `Los Angeles Times 1923`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 3, 
+        {
+            id: 3,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874699/Hidden%20Gable%20Estate/history%20page/stranger-within-our-gates_zghaht.jpg`,
             text: `Los Angeles Evening Express 1926`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 4, 
+        {
+            id: 4,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874699/Hidden%20Gable%20Estate/history%20page/swobdi-feather-hat_stddkw.jpg`,
             text: `Aileen Pringle, Goldwyn Star`,
             text2: `Los Angeles Times 1923`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 5, 
-            url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874699/Hidden%20Gable%20Estate/history%20page/swobdi-chinese-robe_ypeydp.jpg`, 
+        {
+            id: 5,
+            url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874699/Hidden%20Gable%20Estate/history%20page/swobdi-chinese-robe_ypeydp.jpg`,
             text: `Mary Astor, she may be best remembered for her performance as Brigid O'Shaughnessy in The Maltese Falcon (1941)`,
             text2: `Los Angeles Times 1923`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 6, 
+        {
+            id: 6,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874698/Hidden%20Gable%20Estate/history%20page/swobdi-25-years-in-business_uitt4o.jpg`,
             text: `Mrs Swobdi started as a Millinery Shop in 1901`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 7, 
+        {
+            id: 7,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874699/Hidden%20Gable%20Estate/history%20page/LA-fashion-show_a7ix14.jpg`,
             text: `Los Angeles Herald 1917`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 8, 
+        {
+            id: 8,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874699/Hidden%20Gable%20Estate/history%20page/swobdi-illustration_du4jwq.jpg`,
             text: `Extracted from one of Mrs Swobdi's advertisements`,
             text2: `Los Angeles Times 1923`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 9, 
+        {
+            id: 9,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874700/Hidden%20Gable%20Estate/history%20page/swobdi-on-display-for-your-approval_df1vj1.jpg`,
             text: `Los Angeles Evening Express 1926`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 10, 
+        {
+            id: 10,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1736874699/Hidden%20Gable%20Estate/history%20page/swobdi-large-hat_djrnsn.jpg`,
             text: `Los Angeles Times 1920`,
-            alt: '',
+            alt: "",
         },
-        { 
-            id: 11, 
+        {
+            id: 11,
             url: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1737419003/Hidden%20Gable%20Estate/history%20page/swobdi_at_house_-_m_sojiro.jpg`,
             text: `Mrs Swobdi in front of her winter home circa 1928`,
-            alt: '',
+            alt: "",
         },
-    ]
+    ];
 
     return (
-        <div className='swobdi'>
-            <section className='swobdi__text-content'>
+        <div className="swobdi">
+            <section className="swobdi__text-content">
                 <header>
                     <h3>“Roaring Twenties, Liberated Women, and Flappers"</h3>
                     <h4>Winter Residence until 1931</h4>
                 </header>
                 <ul>
-                    <li className='list-item'>
-                        Mrs Agnes Swobdi is credited with pioneering the Los Angeles Fashion District with her original designs and exclusive imports.
+                    <li className="list-item">
+                        Mrs Agnes Swobdi is credited with pioneering the Los
+                        Angeles Fashion District with her original designs and
+                        exclusive imports.
                     </li>
-                    <li className='list-item'>
-                        As an importer, curating her latest collection featured in her many shops, she made numerous trans-Atlantic crossings visiting all of Europe, traveled extensively throughout Asia on the Orient Express and special trips to Japan, Australia and Hawaii.
+                    <li className="list-item">
+                        As an importer, curating her latest collection featured
+                        in her many shops, she made numerous trans-Atlantic
+                        crossings visiting all of Europe, traveled extensively
+                        throughout Asia on the Orient Express and special trips
+                        to Japan, Australia and Hawaii.
                     </li>
-                    <li className='list-item'>
-                        Her most favorite places to visit included the ruins of Rome, the temples of Asia, the lagoons of Mandalay, and the pretty gardens of Japan.
+                    <li className="list-item">
+                        Her most favorite places to visit included the ruins of
+                        Rome, the temples of Asia, the lagoons of Mandalay, and
+                        the pretty gardens of Japan.
                     </li>
-                    <li className='list-item'>
-                        As a couturière, her dresses and hats were worn by Silent Film actresses in the 1920s.
+                    <li className="list-item">
+                        As a couturière, her dresses and hats were worn by
+                        Silent Film actresses in the 1920s.
                     </li>
-                    <li className='list-item'>
-                        Many of her fashions were featured in the 1924 film “Try and forget it” staring Bryant Washburn and directed by Cullen Tate. 
+                    <li className="list-item">
+                        Many of her fashions were featured in the 1924 film “Try
+                        and forget it” staring Bryant Washburn and directed by
+                        Cullen Tate.
                     </li>
-                    <li className='list-item'>
-                        Swobdi's parties were legendary, and certainly included rising stars and Hollywood's elite.
+                    <li className="list-item">
+                        Swobdi's parties were legendary, and certainly included
+                        rising stars and Hollywood's elite.
                     </li>
                 </ul>
             </section>
-            <section className='swobdi__carousel-container'>
+            <section className="swobdi__carousel-container">
                 <p>Click on an image to enlarge</p>
-                <Carousel 
-                    items={images} 
-                    showText={true} 
-                    className='swobdi-carousel'
+                <Carousel
+                    items={images}
+                    showText={true}
+                    className="swobdi-carousel"
                 />
             </section>
         </div>
-    )
-}
+    );
+};
 
 export default Swobdi;
-
-
--- src/components/homePage/Reviews.jsx
+++ src/components/homePage/Reviews.jsx
@@ -1,64 +1,64 @@
-import { useState } from 'react';
-import CarouselControls from '../common/carousel/CarouselControls';
-import './Reviews.css';
-import './ReviewsSlider.css';
+import { useState } from "react";
+import CarouselControls from "../common/carousel/CarouselControls";
+import "./Reviews.css";
 
 const Reviews = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const reviews = [
-        {   id: 1,
+        {
+            id: 1,
             title: "Gorgeous Setting!",
             text: "We used this house for a corporate shoot and our team also stayed at the property, the setting is beautiful and the house is clean, well-stocked, and has a ton of activities. The management crew was extremely responsive to any questions that popped up during our stay. Highly recommend!",
             author: "Corporate Client",
-            date: "Recent Stay"
         },
-        {   id: 2,
+        {
+            id: 2,
             title: "A Magical Palm Spring Oasis",
             text: "I have no words for the most beautiful and magical week we just spent at this property. Well worth every penny. First, the location can't be beat - walking distance to the best restaurants, coffee shops, and shops. We really didn't NEED a car at all - except for a grocery run.",
             author: "Vacation Guest",
-            date: "Recent Stay"
         },
-        {   id: 3,
+        {
+            id: 3,
             title: "The house is BEAUTIFUL",
             text: "The furniture, the decor, windows, grounds, and original features were completely in line with the history of the house. It has a quiet elegance that was perfect. The beds are comfortable and bathrooms are plenty. The view, the pool, and the outdoor gathering areas were the real winners this week - we could have stayed forever. Management was communicative and thorough and quickly dealt with any issues.",
             author: "Vacation Guest",
-            date: "Recent Stay"
-        }
+        },
     ];
 
     const handlePrevious = () => {
-        setCurrentIndex((prevIndex) => 
+        setCurrentIndex(prevIndex =>
             prevIndex === 0 ? reviews.length - 1 : prevIndex - 1
         );
     };
 
     const handleNext = () => {
-        setCurrentIndex((prevIndex) => 
-            (prevIndex + 1) % reviews.length
-        );
+        setCurrentIndex(prevIndex => (prevIndex + 1) % reviews.length);
     };
 
     return (
-        <section className="reviews reviews-slider">
-            <h2 className='reviews__heading'>Guest Reviews</h2>
+        <section className="reviews-section reviews-slider">
+            <h2 className="reviews__heading">Guest Reviews</h2>
             <div className="slider-wrapper">
                 <div className="reviews__slider-container">
-                    <div 
-                        className="reviews__slider" 
-                        style={{ 
+                    <div
+                        className="reviews__slider"
+                        style={{
                             transform: `translateX(-${currentIndex * 100}%)`,
                         }}
                     >
                         {reviews.map((review, index) => (
-                            <div 
-                                key={index} 
+                            <div
+                                key={index}
                                 className="review-card slider-review-card"
                             >
-                                <h3 className="review__title">{review.title}</h3>
+                                <h3 className="review__title">
+                                    {review.title}
+                                </h3>
                                 <p className="review__text">{review.text}</p>
                                 <div className="review__attribution">
-                                    <span className="review__author">{review.author}</span>
-                                    {/* <span className="review__date">{review.date}</span> */}
+                                    <span className="review__author">
+                                        {review.author}
+                                    </span>
                                 </div>
                             </div>
                         ))}
@@ -72,7 +72,7 @@ const Reviews = () => {
                 showPrev={handlePrevious}
                 showNext={handleNext}
                 showDots={true}
-                className={'reviews-carousel'}
+                className={"reviews-carousel"}
             />
         </section>
     );
