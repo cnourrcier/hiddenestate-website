95d25513f1aa2defb8c2d43c9c639e91e58748d0
cnourrcier <cnourrcier@hotmail.com>
2025-05-22 16:03:56 -0500

    Update styles home page location section

--- src/components/homePage/Location.jsx
+++ src/components/homePage/Location.jsx
@@ -1,36 +1,41 @@
-
-import { useState } from 'react';
-import Map, { Marker, Popup, NavigationControl, FullscreenControl, GeolocateControl } from 'react-map-gl';
-import { MapPin } from 'lucide-react';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import './Location.css';
+import { useState } from "react";
+import Map, {
+    Marker,
+    Popup,
+    NavigationControl,
+    FullscreenControl,
+    GeolocateControl,
+} from "react-map-gl";
+import { MapPin } from "lucide-react";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./Location.css";
 
 const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const propertyLocation = {
-    latitude: 33.83140,
+    latitude: 33.8314,
     longitude: -116.54887,
-    zoom: 13
+    zoom: 13,
 };
 
 const initialViewLocation = {
-    latitude: 33.8310,
+    latitude: 33.831,
     longitude: -116.54887,
-    zoom: 13
-}
+    zoom: 13,
+};
 
 const markerInfo = {
     longitude: propertyLocation.longitude,
     latitude: propertyLocation.latitude,
     name: "Hidden Gable Estate",
-    address: "222 W Chino Dr. Palm Springs, CA 92262"
+    address: "222 W Chino Dr. Palm Springs, CA 92262",
 };
 
 const mapStyles = [
     { name: "Streets", url: "mapbox://styles/mapbox/streets-v11" },
     { name: "Satellite", url: "mapbox://styles/mapbox/satellite-v9" },
     { name: "Light", url: "mapbox://styles/mapbox/light-v10" },
-    { name: "Dark", url: "mapbox://styles/mapbox/dark-v10" }
+    { name: "Dark", url: "mapbox://styles/mapbox/dark-v10" },
 ];
 
 const CustomMarker = ({ onClick }) => (
@@ -53,7 +58,7 @@ const Location = () => {
                 <div className="map-container">
                     <Map
                         initialViewState={initialViewLocation}
-                        style={{ width: '100%', height: 400 }}
+                        style={{ width: "100%", height: 400 }}
                         mapStyle={currentStyle}
                         mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
                     >
@@ -70,11 +75,14 @@ const Location = () => {
                                 longitude={popupInfo.longitude}
                                 latitude={popupInfo.latitude}
                                 closeOnClick={false}
-                                className='custom-popup'
+                                className="custom-popup"
                             >
-                                <div className='popupInfo'>
-                                    <img src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1725299251/Hidden%20Gable%20Estate/home%20page/_I1A0197m_2_oiuer9.jpg`} alt='Property' />
-                                    <div className='popup-content'>
+                                <div className="popupInfo">
+                                    <img
+                                        src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1725299251/Hidden%20Gable%20Estate/home%20page/_I1A0197m_2_oiuer9.jpg`}
+                                        alt="Property"
+                                    />
+                                    <div className="popup-content">
                                         <h3>{popupInfo.name}</h3>
                                         <p>{popupInfo.address}</p>
                                     </div>
@@ -82,16 +90,18 @@ const Location = () => {
                             </Popup>
                         )}
 
-                        <NavigationControl position='top-right' />
-                        <FullscreenControl position='top-right' />
-                        <GeolocateControl position='top-right' />
+                        <NavigationControl position="top-right" />
+                        <FullscreenControl position="top-right" />
+                        <GeolocateControl position="top-right" />
                     </Map>
-                    <div className='map-style-switcher'>
+                    <div className="map-style-switcher">
                         {mapStyles.map((style, index) => (
                             <button
                                 key={index}
                                 onClick={() => setCurrentStyle(style.url)}
-                                className={currentStyle === style.url ? 'active' : ''}
+                                className={
+                                    currentStyle === style.url ? "active" : ""
+                                }
                             >
                                 {style.name}
                             </button>
@@ -99,24 +109,33 @@ const Location = () => {
                     </div>
                 </div>
 
-                <div className='side-info-container'>
-                    <div className='side-info'>
-                        <img src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1725299251/Hidden%20Gable%20Estate/home%20page/_I1A0197m_2_oiuer9.jpg`} alt='Property' />
-                        <div className="side-info-overlay"></div>
-                        <div className='side-info-content'>
+                <div className="side-content-container">
+                    <div className="side-content">
+                        <img
+                            src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1725299251/Hidden%20Gable%20Estate/home%20page/_I1A0197m_2_oiuer9.jpg`}
+                            alt="Property"
+                        />
+                        <div className="side-content-text">
                             <h3>{markerInfo.name}</h3>
                             <p>{markerInfo.address}</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='location__description'>
+            <div className="location-description">
                 <p>
-                    You'll be within a short walking distance of all the best dining and entertainment, nestled in Old Las Palmas, a premier neighborhood referred to as the <span>"Beverly Hills"</span> of Palm Springs. Rich in architecture and Hollywood history, this affluent neighborhood also holds estates belonging to well-known figures like Leonardo DiCaprio, Kurt Russel, and Katherine Hepburn.
+                    You'll be within a short walking distance of all the best
+                    dining and entertainment, nestled in{" "}
+                    <span>Old Las Palmas</span>, a premier neighborhood referred
+                    to as the <span>"Beverly Hills"</span> of Palm Springs. Rich
+                    in architecture and Hollywood history, this affluent
+                    neighborhood also holds estates belonging to well-known
+                    figures like Leonardo DiCaprio, Kurt Russel, and Katherine
+                    Hepburn.
                 </p>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Location;
