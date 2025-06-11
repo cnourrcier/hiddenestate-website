9a357de21137afc2eef40bc816cb2ff041ba86df
cnourrcier <cnourrcier@hotmail.com>
2025-06-10 19:27:50 -0500

    Add return link in foundation psusd event gallery

--- src/pages/Galleries/FoundationForPsusd2025.jsx
+++ src/pages/Galleries/FoundationForPsusd2025.jsx
@@ -1,10 +1,14 @@
 import ImageGallery from "../../components/common/ImageGallery/ImageGallery";
 import { foundationEventGalleryImages } from "../../data/imagesData";
+import { Link } from "react-router-dom";
 import "./FoundationForPsusd2025.css";
 
 const FoundationForPsusd2025 = () => {
     return (
         <section className="foundation-event-gallery-page">
+            <Link to={"/galleries/events"} className="return-link">
+                <span className="return-link-arrow">←</span> Go back
+            </Link>
             <ImageGallery images={foundationEventGalleryImages} />
         </section>
     );
